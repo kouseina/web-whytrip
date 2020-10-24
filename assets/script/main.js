a131a77d14e174d848b14e02128e1c064279f745
alfinabalqis <alfinabalqis28@gmail.com>
2020-10-24 12:44:25 +0700

    revisi

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,3 +1,21 @@
+
+function myFunction(){
+    var x = document.getElementById("myInput");
+    var y = document.getElementById("hide");
+    var z = document.getElementById("hide2");
+
+    if(x.type === 'password'){
+        x.type = "text";
+        y.style.display = "block";
+        z.style.display = "none";
+    }
+    else {
+        x.type = "text";
+        y.style.display = "none";
+        z.style.display = "block";
+    }
+}
+
 const nextIcon = '<img src="assets/img/right-arrow.svg" alt="right" class="img-arrow">';
 const prevIcon = '<img src="assets/img/left-arrow.svg" alt="left" class="img-arrow">';
 
